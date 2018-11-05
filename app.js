43c3a07cb48eb93b6694be49e964b5187d448f00
Kasia902 <khrystyna.shumska@gmail.com>
2018-11-05 18:06:38 +0200

    done 7 exercises from 'Java Script Language Basics 1' lecture

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-console.log("\nNice work! Your first application have been launched.");
-console.log("Now, I need to know your name, please type it below: ");
-
-let stdin = process.openStdin();
-
-stdin.addListener("data", (txt) => {
-    if(txt.toString().trim() === "quit") {
-        console.log("\nHave a nice JS trip! Bye-bye");
-        stdin.end();
-    } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
-    }   
-  });
+//1. Write a JS program to compute and print the sum of the two given integers
+let a=2;
+let b=34;
+console.log (a+b);
+//2. Write a JS program to create a new string adding “Test_" in front of a given string
+let string1="text";
+let string2="Test_"+string1;
+console.log (string2);
+//3. Write a JS program to combine and print your first, last name and age
+let firstname="Khrystyna";
+let lastname="Shumska";
+let age=30;
+console.log ("My name is " + firstname +" " + lastname + " and I am " + age +" years old");
+//4. Write a JS program to check a given integer is bigger then 100
+let a=30;
+console.log (a>100);
+//5. Write a JS program to find if the first number is larger from the two given positive integers
+let a=30;
+let b=789;
+console.log (a>b);
+//6. Write a JS program to check two given numbers and return true if one of the number is 50 or if their sum is 50
+let a=30;
+let b=21;
+console.log((a == 50 || b == 50) || (a+b == 50));
+//7. Write a JS program check if a given positive number is a multiple of 3 or a multiple of 7
+let a=70;
+console.log("multiple of 3 " + (a%3 == 0));
+console.log("multiple of 7 " + (a%7 == 0));
