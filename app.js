e46cf5b4b380bdc6daa7b8ffa44f7f32d1139976
Kasia902 <khrystyna.shumska@gmail.com>
2018-11-08 13:59:08 +0200

    Done tasks from Java Script Language Basics 2

--- app.js
+++ app.js
@@ -1,28 +1,86 @@
-//1. Write a JS program to compute and print the sum of the two given integers
-let a=2;
-let b=34;
-console.log (a+b);
-//2. Write a JS program to create a new string adding �Test_" in front of a given string
-let string1="text";
-let string2="Test_"+string1;
-console.log (string2);
-//3. Write a JS program to combine and print your first, last name and age
-let firstname="Khrystyna";
-let lastname="Shumska";
-let age=30;
-console.log ("My name is " + firstname +" " + lastname + " and I am " + age +" years old");
-//4. Write a JS program to check a given integer is bigger then 100
-let c=30;
-console.log (c>100);
-//5. Write a JS program to find if the first number is larger from the two given positive integers
-let d=30;
-let e=789;
-console.log (d>e);
-//6. Write a JS program to check two given numbers and return true if one of the number is 50 or if their sum is 50
-let f=30;
-let g=21;
-console.log((f == 50 || g == 50) || (f+g == 50));
-//7. Write a JS program check if a given positive number is a multiple of 3 or a multiple of 7
-let h=70;
-console.log("multiple of 3 " + (h%3 == 0));
-console.log("multiple of 7 " + (h%7 == 0));
+//1. Write a JavaScript function to check if given string includes given symbol.
+let str1= " A B dlfkjhgdfljh jhdkhkjh jbkjhjkh";
+  function checksymbol2 (String){
+      let result=/d/g.test(str1);
+    return result;
+  }
+  let a= checksymbol2 (str1);
+  console.log (`Contains: ${a}`);
+
+  //2. Write a JavaScript function to check whether a string is blank or not.
+  let str2="fdhtr ";
+  function checkIfBlank (String){
+      let result=String.replace (" ", "");
+      if (result.length > 0) 
+      return false;
+      else 
+      return true;
+  }
+  let b=checkIfBlank (str2);
+  console.log (`Blank: ${b}`);
+
+//3. Write a JavaScript function to convert a string in abbreviated form. console.log(abbrev (“Nazar Babii")) – should be “N.B.”
+let str3="khrystyna shumska";
+function abbreviate(String){
+    let splitted=String.split (' ');
+if (splitted.length > 1) {
+    return (splitted[0].charAt(0) + "." + splitted[1].charAt(0) + "."); 
+}
+}
+let c=abbreviate(str3);
+console.log(`Abbreviation of given name is: ${c.toUpperCase()}`);
+
+//4. Write a JavaScript function that accept two integers and display the larger.
+let d=167;
+let e=277;
+function biggerNumber (d,e){
+    if (d>e){
+        result=d;
+        //console.log (`${a} is bigger than ${b}`);
+        return result;
+    }
+    else if (d===e){
+        result=d;
+    //console.log (`${a} is equal to ${b}`);
+    return result;
+    }
+    else {
+        result=e;
+    //console.log (`${b} is bigger than ${a}`);
+    return result;
+    }
+}
+let f= biggerNumber(d,e);
+console.log(`Bigger number is ${f}`);
+
+//5. Write a JavaScript function with conditional statement to sort three numbers.
+let num1=23;
+let num2=34;
+let num3=1;
+function sortNumbers(){
+    if (num1>num2 && num1>num3){
+        if (num2>num3){
+        console.log(`Sorted numbers: ${num1}, ${num2}, ${num3}`);
+        }
+        else {
+            console.log (`Sorted numbers: ${num1}, ${num3}, ${num2}`);
+        }
+    }
+    if (num2>num1 && num2>num3){
+        if (num1>num3){
+        console.log(`Sorted numbers: ${num2}, ${num1}, ${num3}`);
+        }
+        else {
+            console.log (`Sorted numbers: ${num2},${num3},${num1}`);
+        }
+    }
+    if (num3>num1 && num3>num2){
+        if (num1>num2){
+        console.log(`Sorted numbers: ${num3},${num1},${num2}`);
+        }
+        else {
+            console.log(`Sorted numbers: ${num3}, ${num2},${num1}`)
+        }
+    }
+}
+sortNumbers(num1,num2,num3);
