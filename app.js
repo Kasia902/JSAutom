3d82d6e6458eb62fc2f90879f4c3895adaedbaca
Kasia902 <khrystyna.shumska@gmail.com>
2018-11-13 11:44:59 +0200

    Done tasks from Java Script Language Basics 3

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-console.log("\nNice work! Your first application have been launched.");
-console.log("Now, I need to know your name, type it below: ");
+//1. Write a JavaScript function to get the last element of an array
+function lastElement(array)
+{
+    let n = ( array.length - 1 );
+    //console.log(n);
+    console.log( array.slice(n) );
+}
+lastElement([ 23, 44, 14, 3, 5, 7 ]);
 
-let stdin = process.openStdin();
+//2. Write a simple JavaScript program to join all elements of the following array into a string
+function convertArrayToString(array)
+{
+    console.log( array.toString() );
 
-stdin.addListener("data", (txt) => {
-    if(txt.toString().trim() === "quit") {
-        console.log("\nHave a nice JS trip! Bye-bye");
-        stdin.end();
-    } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
-    }   
-  });
+    console.log( array.join(";") );
+}
+convertArrayToString([ 23, 44, 14, 3, 5, 7 ]);
+
+//3. There are two arrays with individual values, 
+//write a JavaScript program to compute the sum of each individual index value from the given arrays and save them to third array
+function sumArraysElements(array1, array2)
+{
+    for ( let i = 0; i < array1.length; i++ )
+    {
+        console.log( array1[i] + array2[i] );
+    }
+}
+
+let a = [ 1, 4, 5, 6 ];
+let b = [ 3, 7, 34, 1 ];
+let c = sumArraysElements(a, b);
+
+//4. Write a JavaScript program to calculate the area and perimeter of a rectangle. 
+//Rectangle should be an object with properties width and height and methods getArea() and getPerimeter();
+
+let rectangle = {
+    width: 12,
+    height: 4,
+    area: function getArea() { return this.width * this.height; },
+    perimeter: function getPerimeter() { return ( this.width * 2 ) + ( this.height * 2 ); }
+};
+console.log(`Area is: ${rectangle.area()}`);
+console.log(`Perimeter is: ${rectangle.perimeter()}`);
