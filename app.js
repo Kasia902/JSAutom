844bfbf0f4041531810dc78aa02e11197fc608b7
Kasia902 <khrystyna.shumska@gmail.com>
2018-11-26 17:37:08 +0200

    done 1 task

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-console.log("\nNice work! Your first application have been launched.");
-console.log("Now, I need to know your name, type it below: ");
-
-let stdin = process.openStdin();
-
-stdin.addListener("data", (txt) => {
-    if(txt.toString().trim() === "quit") {
-        console.log("\nHave a nice JS trip! Bye-bye");
-        stdin.end();
-    } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2018. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`); 
-    }   
+ let Promise1 = new Promise((resolve, reject) => 
+setTimeout(() => {
+    console.log(`1st step`);
+    resolve();
+  })).then(() => {
+        console.log(`2nd step`);
   });
+
+let Promise2 = new Promise((resolve, reject) => 
+  setTimeout(() => {
+      console.log(`3rd step`);
+      resolve();
+    }, 3000)).then(() => {
+          console.log(`4th step`);
+    });
+
+let doPromises = Promise1.then(function() {
+        return Promise2;
+       });
+console.log(doPromises); 
